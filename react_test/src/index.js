807cdec3dbe278cd7adf54a365049a8e3f5b9284
Thomas Clarke <TClarke@scottlogic.com>
2018-01-05 12:12:37 +0000

    Board updating

--- react_test/src/index.js
+++ react_test/src/index.js
@@ -3,23 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      value: props.value,
-    };
-  }
-
   render() {
     return (
-      <button className="square" onClick={() => {
-          this.setState({
-            value: (this.state.value === "X") ? "O" : "X"
-          })
-        }
-      }
-      >
-        {this.state.value}
+      <button className="square" onClick={() => this.props.onClick()}>
+        {this.props.value}
       </button >
     );
   }
@@ -34,12 +21,22 @@ class Board extends React.Component {
     };
   
     for(let i = 0; i < this.state.squares.length; ++i){
-      this.state.squares[i] = i;
+      this.state.squares[i] = i+1;
     }
   }
 
+  handleClick = function(i){
+    const squares = [...this.state.squares];
+    squares[i] = this.state.player;
+    this.setState({
+      player: this.state.player === "X" ? "O" : "X",
+      squares: squares
+    });
+  }
+
   renderSquare(i) {
-    return <Square value={this.state.squares[i]} />;
+    return <Square value={this.state.squares[i]}  onClick={() => this.handleClick(i)}
+  />;
   }
 
   render() {
@@ -76,7 +73,7 @@ class Game extends React.Component {
           <Board />
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
+          <div>{"Unending"}</div>
           <ol>{/* TODO */}</ol>
         </div>
       </div>

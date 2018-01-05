b59a79fee076a8e6e677f0bcd32706df8a7f8ce0
Thomas Clarke <TClarke@scottlogic.com>
2018-01-05 15:14:52 +0000

    Buttons!

--- react_test/src/index.js
+++ react_test/src/index.js
@@ -51,6 +51,26 @@ class Game extends React.Component {
     };
   }
 
+  calculateWinner = function (squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
   handleClick = function (i) {
     console.info("CLICKED!", i);
 
@@ -61,8 +81,8 @@ class Game extends React.Component {
 
       let player = this.state.player;
       let winner = true;
-      if(!calculateWinner(squares)){
-        player = ((this.state.player === "X" ) ? "O" : "X")
+      if (!this.calculateWinner(squares)) {
+        player = ((this.state.player === "X") ? "O" : "X")
         winner = false;
       }
 
@@ -80,7 +100,7 @@ class Game extends React.Component {
     const history = this.state.history;
     const current = history[history.length - 1];
 
-    const winner = calculateWinner(current);
+    const winner = this.calculateWinner(current);
     let status;
     if (winner) {
       status = 'Winner!      ' + this.state.player;
@@ -88,15 +108,25 @@ class Game extends React.Component {
       status = 'Next player: ' + this.state.player;
     }
 
+    const moves = history.map((step, move) => {
+      const desc = move ?
+        'Go to move #' + move :
+        'Go to game start';
+      return (
+        <li>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      );
+    });
+
     return (
       <div className="game">
         <div className="game-board">
-          <div className="status">{status}</div>
           <Board squares={current} onClick={(i) => this.handleClick(i)} player={this.state.player} />
         </div>
         <div className="game-info">
-          <div>{/*state*/}</div>
-          <ol>{/* TODO */}</ol>
+          <div className="status">{status}</div>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
@@ -108,23 +138,3 @@ ReactDOM.render(
   <Game />,
   document.getElementById('root')
 );
-
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
-}
